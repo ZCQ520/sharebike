70719922db5874178c73efd8cb8a9a65c2bad244
laowang123 <w584839880@gmail.com>
2017-10-16 19:14:46 +0800

    no message

---
+++ sharebike-api/src/main/java/com/wzu/ShareBikeApplication.java
@@ -0,0 +1,16 @@
+package com.wzu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jack
+ * @date 2017/10/16   18:39
+ */
+@SpringBootApplication
+public class ShareBikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareBikeApplication.class, args);
+    }
+}
---
+++ sharebike-common/src/main/java/com/wzu/pojo/user/User.java
@@ -0,0 +1,38 @@
+package com.wzu.pojo.user;
+
+/**
+ * @author jack
+ * @date 2017/10/16   17:15
+ */
+public class User {
+
+    private String id;
+
+    private Integer age;
+
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
